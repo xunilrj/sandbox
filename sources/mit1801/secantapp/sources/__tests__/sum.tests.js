d0cf5d43ab7577e7807663bb06fcc05a9a37d798
Daniel Leite <xunilrj@hotmail.com>
2016-08-20 22:30:36 -0300

    jest and react renderer working

--- sources/mit1801/secantapp/sources/__tests__/sum.tests.js
+++ sources/mit1801/secantapp/sources/__tests__/sum.tests.js
@@ -1,8 +1,16 @@
 jest.unmock('../libs/sum'); // unmock to use the actual implementation of sum
+jest.unmock('react');
+jest.unmock('react-test-renderer');
 
 describe('sum', () => {
   it('adds 1 + 2 to equal 3', () => {
     const sum = require('../libs/sum');
     expect(sum(1, 2)).toBe(3);
+  });
+
+  it('renders correctly', () => {
+    var React = require('react');
+    var d = <div>Facebook</div>;
+    //expect(tree).toMatchSnapshot();
   });
 });
---
+++ sources/mit1801/secantapp/sources/components/secantcomponent.js
@@ -0,0 +1,13 @@
+var React = require('React');
+
+var SecantComponent = React.createClass({
+    render:function(){
+            return (
+            <div>
+                <intput type="checkbox" />
+                <button>Calculate</button>
+            </div>);
+    }
+});
+
+module.exports = SecantComponent;
