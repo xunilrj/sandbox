8806b4a890b262951c46a06cc76e97311c7b8a36
Daniel Leite <xunilrj@hotmail.com>
2016-09-09 20:32:18 -0300

    very simple ps1 script to test the api

---
+++ sources/playframework/webmap/test/CarApiTest.ps1
@@ -0,0 +1,6 @@
+﻿invoke-restmethod -Method Post -Uri http://localhost:9000/api/car -Body '{"name":"car1","latitude":4.0,"longitude":5.0}' -ContentType "application/json"
+invoke-restmethod -Method Get -Uri http://localhost:9000/api/car/car1
+invoke-restmethod -Method Put -Uri http://localhost:9000/api/car/car1 -Body '{"name":"car1","latitude":1.0,"longitude":2.0}' -ContentType "application/json"
+invoke-restmethod -Method Get -Uri http://localhost:9000/api/car/car1
+invoke-restmethod -Method Put -Uri http://localhost:9000/api/car/car1 -Body '{"latitude":1.0,"longitude":3.0}' -ContentType "application/json"
+invoke-restmethod -Method Get -Uri http://localhost:9000/api/car/car1
