353f8e4accb02b99fd43f4a27bbdbc0be0f0f05c
Daniel Leite <Daniel Leite>
2017-02-12 17:19:58 +0000

    edx ridge regression and active learning

--- sources/course-edx-datamining/hw1_regression.py
+++ sources/course-edx-datamining/hw1_regression.py
@@ -4,14 +4,14 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 import sys
-import csv
 import numpy
+from sklearn.preprocessing import StandardScaler
 
 l = 2
 if(len(sys.argv) > 2):
     l = int(sys.argv[1])
     
-s2 = 0
+s2 = 1
 if(len(sys.argv) > 3):
     s2 = int(sys.argv[2])
 
@@ -33,19 +33,38 @@ print("xtrain:", xtrain)
 print("ytrain:", ytrain)
 print("xtest:", xtest)
 
+#WRR
 
 Xmatrix = numpy.loadtxt(open(xtrain, "rb"), delimiter=",", skiprows=0)
+#Xmatrix = StandardScaler().fit_transform(Xmatrix)
 XmatrixT = numpy.transpose(Xmatrix)
 Ymatrix = numpy.loadtxt(open(ytrain, "rb"), delimiter=",", skiprows=0)
+#Ymatrix = Ymatrix - numpy.average(Ymatrix)
 
-xTx = numpy.multiply(XmatrixT, Xmatrix)
+xTx = numpy.dot(XmatrixT,Xmatrix)
 lambdaMatrix = l *  numpy.identity(xTx.shape[0])
 
-tobeInverted = lambdaMatrix + xTx
+tobeInverted = numpy.add(lambdaMatrix,xTx)
 inverted = numpy.linalg.inv(tobeInverted)
-wrr = numpy.multiply(numpy.multiply(inverted, XmatrixT), Ymatrix)
+wrr = numpy.dot(numpy.dot(inverted, XmatrixT), Ymatrix)
 
+#ACTIVE
 
+#sumxixiT = sum([Xmatrix[:][i]*numpy.transpose(Xmatrix[:][i]) for i in range(0, Xmatrix.shape[0])])
 
-    
-print("Argument List:", str(sys.argv))
+SIGMA = numpy.linalg.inv(lambdaMatrix + (1/s2)*xTx)
+def s02Value(i):
+    return numpy.dot(numpy.dot(numpy.transpose(Xmatrix[:][i]),SIGMA),Xmatrix[:][i])
+si2s = [(i,s02Value(i)) for i in range(0, Xmatrix.shape[0])]
+si2sdesc = sorted(si2s, key=lambda tup: tup[1])[0:10]
+
+f = open('wRR_' + str(l) + ".csv", 'w')
+for i in range(0, wrr.shape[0]):
+    f.write(str(wrr[i]) + "\n")
+f.close()
+
+f = open('active_' + str(l) + "_" + str(s2) + ".csv", 'w')
+for i in range(0, 9):
+    f.write(str(si2sdesc[i][0]) + ",")
+f.write(str(si2sdesc[9][0]))
+f.close()
