3b267ee9449e3a3cd046681141eac4a80262f170
Daniel Leite <xunilrj@hotmail.com>
2017-03-30 17:03:20 +0100

    powershell oneliners

--- sources/Powershell/Modules/OneLiners/OneLiners.psm1
+++ sources/Powershell/Modules/OneLiners/OneLiners.psm1
@@ -18,3 +18,22 @@ function %% {
         $currentValue
     }
 }
+
+function ??? {
+    [CmdletBinding()]
+    param([Parameter(Mandatory=$true,Position=0)][Alias("e")]$regexp, [Parameter()][Alias("v")][switch]$NotMatch, [Parameter(ValueFromPipeline = $true)]$PSItem)
+    begin{
+        $Regex = [System.Text.RegularExpressions.Regex]::new($regexp,"ExplicitCapture")
+    }
+    process{
+        $result = $Regex.Match($Regex)
+        if($result.Success -eq $true){
+            $newobj = New-Object PSCustomObject
+            $Regex.GetGroupNames() | ? {$_ -notmatch "\d+"} | % { 
+                $newobj = $newobj | Add-Member -Name $_ -Value ($result[$_].Value) -MemberType NoteProperty
+            }
+            $newobj 
+        }
+    }
+}
+cat "C:\github\xunilrj-mardocs\MarDocs\MarDocs.Core\Models\DrySpotFixture\DrySpotFixture.cs" | ??? "public string (?<NAME>.*?)"
