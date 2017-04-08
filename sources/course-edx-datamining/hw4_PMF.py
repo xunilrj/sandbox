444a16f7c64ac564d674d9cf19a9ca0798c3887c
Daniel Leite <Daniel Leite>
2017-04-08 19:23:52 +0100

    probabilistic matrix factorization in python

--- sources/course-edx-datamining/hw4_PMF.py
+++ sources/course-edx-datamining/hw4_PMF.py
@@ -36,18 +36,34 @@ uM = numpy.reshape(uM, (d, moviesQtd))
 vM = numpy.matrix(numpy.repeat(0, d * moviesQtd))
 vM = numpy.reshape(vM, (d, moviesQtd))
 
+for j in range(0,moviesQtd):
+    for ix in range(0, d):
+            vM[j,ix] = numpy.random.normal(0,1)
+
 def I(n):
     return numpy.matrix(numpy.identity(n))
 def T(v):
     return numpy.transpose(v)
 
+def printMatrix(path, matrix2print):
+    f = open(path, 'w')
+    shape = numpy.shape(matrix2print)
+    for row in range(0, shape[0]):
+        for col in range(0, shape[1]):
+            f.write('%.5f' % matrix2print[row][col])
+            if(col != shape[1]-1):
+                f.write(",")
+        f.write("\n")
+    f.close()
+
+fobj = open("objectives.csv", 'w')
 iteration = 0
-for iteration in range(0,iterations):
+for iteration in range(0,iterations):    
     for i in range(0,usersQtd):
         #u_i = \big[\lambda\sigma^2I + \sum_{j\in \omega ui}{v_jv^T}\big]*\big[\sum_{j\in \omega u_i}{M_{ij}v_j}\big]
         indices = numpy.where(ratingsM[:,0] == i)
         uiRatings = ratingsM[indices]
-        js = numpy.unique(uiRatings[:,1])
+        js = numpy.unique(uiRatings[:,0])
         sumofvjvjt = numpy.sum([numpy.dot(vM[:,int(j)],T(vM[:,int(j)])) for j in js], 0)
         item1 = (lambdaa*sigma2)*I(d)
         itemiI = numpy.linalg.inv(item1 + sumofvjvjt)
@@ -55,9 +71,42 @@ for iteration in range(0,iterations):
         result = numpy.dot(itemiI, item2) 
         for ix in range(0, numpy.size(result)):
             uM[i,ix] = result[ix]
-        
-#for j in range(0,moviesQtd):
-        #print(j)
+    for j in range(0,moviesQtd):
+        indices = numpy.where(ratingsM[:,1] == i)
+        uiRatings = ratingsM[indices]
+        iss = numpy.unique(uiRatings[:,1])
+        sumofujujt = numpy.sum([numpy.dot(uM[:,int(i)],T(uM[:,int(i)])) for i in iss], 0)
+        item1 = (lambdaa*sigma2)*I(d)
+        itemiI = numpy.linalg.inv(item1 + sumofujujt)
+        item2 = numpy.sum([M[int(i),int(j)]*uM[:,int(i)] for i in iss], 0)
+        result = numpy.dot(itemiI, item2) 
+        for ix in range(0, numpy.size(result)):
+            vM[i,ix] = result[ix]
+    def objfirst(i,j):
+        dot = (M[i,j] - numpy.dot(T(uM[:,i]),vM[:,j]))
+        dot = dot*dot
+        dot = 1/(2.0*sigma2)
+        return dot
+    def objsecond(i):
+        ui = uM[:,1]
+        ui = numpy.linalg.norm(ui) * numpy.linalg.norm(ui)
+        return (lambdaa/2.0)*ui
+    def objthird(i):
+        vi = vM[:,1]
+        vi = numpy.linalg.norm(vi) * numpy.linalg.norm(vi)
+        return (lambdaa/2.0)*vi
+    objfirstpart = numpy.sum([objfirst(i,j) for i in range(0,usersQtd) for j in range(0,moviesQtd)])
+    objsecondpart = numpy.sum([objsecond(i) for i in range(0,usersQtd)])
+    objthirdpart = numpy.sum([objthird(j) for j in range(0,moviesQtd)])
+    obj = objfirstpart + objsecondpart + objthirdpart
+    fobj.write('%.5f' % obj)
+    fobj.write("\n")
+    if(iteration == 10 or iteration == 25 or iteration == 50):
+        printMatrix("U-" + iteration + ".csv", uM)
+        printMatrix("V-" + iteration + ".csv", vM)
 
-#print(usersM)
-#print(moviesM)
+fobj.close()
+result = numpy.dot(T(vM),uM)
+
+print(result)
+
