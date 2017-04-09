822320f59c3e667404b0e074d4d1d2f04e2f098d
Daniel Leite <Daniel Leite>
2017-04-09 15:39:38 +0100

    K-means and EM-GMM in python

--- sources/course-edx-datamining/hw3_clustering.py
+++ sources/course-edx-datamining/hw3_clustering.py
@@ -6,7 +6,7 @@ os.path.dirname(os.path.abspath(__file__))
 import sys
 import numpy
 
-data = "X.csv"
+data = "X_train.2.csv"
 if(len(sys.argv) >= 2):
     data = sys.argv[1]
  
@@ -40,6 +40,7 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
+from scipy.stats import multivariate_normal
 
 
 dataM = numpy.loadtxt(open(data, "rb"), delimiter=",", skiprows=0)
@@ -50,26 +51,72 @@ sds = numpy.std(dataM, axis=0)
 maxs = numpy.max(dataM, axis=0)
 mins = numpy.min(dataM, axis=0)
 
-colors = ["r","b","g","y","black"]
+colors = ["r","g","b","y","black"]
 
-def plotContour(i, centroid):
+def plotContour(i, centroid, params):
     delta = 0.1
     x = numpy.arange(mins[0], maxs[0], delta)
     y = numpy.arange(mins[1], maxs[1], delta)
     X, Y = numpy.meshgrid(x, y)
-    Z = mlab.bivariate_normal(X, Y, 1.0, 1.0, centroid[0], centroid[1])
+    Z = mlab.bivariate_normal(X, Y,
+          params[0], params[1],
+          centroid[0], centroid[1])
     plt.contour(X, Y, Z, colors=colors[i])
     
-kn = 5
-centroids = [[numpy.random.normal(means[0],sds[0]),
-              numpy.random.normal(means[1],sds[1])]
+n = numpy.shape(dataM)[0]
+d = numpy.shape(dataM)[1]
+kn = 3
+def getRandCentroid():
+    return [numpy.random.normal(means[i],sds[i]) for i in range(0,d)]
+
+centroids = [getRandCentroid()
     for ki
     in range(0,kn)]
+params = [numpy.ones(d) for ki in range(0,kn)]
 
-iterations = 1
+def xi(i):
+    return numpy.reshape(numpy.matrix(dataM[i,:]), (1,d))
+def likelihood(xi,means,sds):
+    return multivariate_normal(means,sds).pdf(xi)
+def pxigivenk(i,k):
+    return likelihood(xi(i),centroids[k],params[k])
+def pofk(k):
+    return 1.0/kn;
+def pkgivenxi(i,k):
+    numerator = pxigivenk(i,k)*pofk(k)
+    denominator = numpy.sum([pxigivenk(i,ck)*pofk(ck)
+        for ck 
+        in range(0,kn)])
+    return numerator/denominator
+def clusterColors():
+    return [
+        numpy.reshape(numpy.matrix([1.0,0.0,0.0]),(1,3)),
+        numpy.reshape(numpy.matrix([0.0,1.0,0.0]),(1,3)),
+        numpy.reshape(numpy.matrix([0.0,0.0,1.0]),(1,3))]
+def clusterProbColors(probs):
+    sums = numpy.sum(probs)
+    colors = clusterColors()
+    result = numpy.sum([(probs[i]/sums)*colors[i] for i in range(0,kn)],0) 
+    return result[0]
+def pointsColor(dataM,cM):    
+    return [clusterProbColors(cM[i,:]) for i in range(0,n)]
+    
+iterations = 50
 for i in range(0,iterations):
-    plt.scatter(dataM[:,0],dataM[:,1])
-    for x in range(0,kn):
-        plotContour(x, centroids[x])
-        plt.savefig("state_" + "{:0>2}".format(i))    
+    print(str(i))
+    cM = numpy.zeros((n,kn))
+    for row in range(0,n):
+        for k in range(0,kn):
+            cM[row,k] = pkgivenxi(row,k)
+    ksums = numpy.sum(cM, axis=0)    
+    plt.figure()
+    plt.scatter(dataM[:,0],dataM[:,1], c=pointsColor(dataM,cM))
+    for k in range(0,kn):
+        plotContour(k, centroids[k], params[k])
+    plt.savefig("state_" + "{:0>2}".format(i))    
+    for k in range(0,kn):
+        for di in range(0,d):
+            result = numpy.sum(dataM[:,di]*cM[:,k])/ksums[k]
+            centroids[k][di] = result
+            params[k][di] = numpy.sum((dataM[:,di]-result)*(dataM[:,di]-result)*cM[:,k])/ksums[k]    
 print("ok")
