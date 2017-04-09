b02eb1fd6b09b7f3afad61e492e45cc74c79a069
Daniel Leite <Daniel Leite>
2017-04-09 12:11:43 +0100

    K-means and EM-GMM in python

--- sources/course-edx-datamining/hw3_clustering.py
+++ sources/course-edx-datamining/hw3_clustering.py
@@ -6,70 +6,15 @@ os.path.dirname(os.path.abspath(__file__))
 import sys
 import numpy
 
-ratings = "X.csv"
+data = "X.csv"
 if(len(sys.argv) >= 2):
-    ratings = sys.argv[1]
+    data = sys.argv[1]
  
-print(ratings)
-
-d = 5 
-sigma2 = 1.0/10.0
-lambdaa = 2.0 
-iterations = 50
+print(data)
 
 def printMatrixShape(name, M):
     print(name + ": " + str(numpy.shape(M)) + " (rows,columns)")
-
-ratingsM = numpy.loadtxt(open(ratings, "rb"), delimiter=",", skiprows=0)
-printMatrixShape("ratingsM", ratingsM)
-
-usersA = numpy.unique(ratingsM[:,0])
-N1 = int(numpy.max(usersA)+1)
-moviesA = numpy.unique(ratingsM[:,1])
-N2 = int(numpy.max(moviesA)+1)
-
-M = numpy.matrix(numpy.repeat(0.0, N1 * N2))
-M = numpy.reshape(M, (N1, N2))
-
-printMatrixShape("M", M)
-
-def omega():
-    return [[i,j] for i in range(0,N1) for j in range(0,N2) if M[i,j] > 0.0]
-def omegaui(i):
-    return [indices[1] for indices in omega() if indices[0] == i]
-def omegavi(i):
-    return [indices[0] for indices in omega() if indices[1] == i]
-
-for rating in ratingsM:
-    M[int(rating[0]),int(rating[1])] = rating[2]
-
-uM = numpy.matrix(numpy.repeat(0.0, N1 * d))
-uM = numpy.reshape(uM, (N1, d))
-
-vM = numpy.matrix(numpy.repeat(0.0, d * N2))
-vM = numpy.reshape(vM, (d, N2))
-
-printMatrixShape("uM", uM)
-printMatrixShape("vM", vM)
-
-def ui(i):
-    return numpy.reshape(numpy.matrix(uM[i,:]), (1,d))
-def setUi(i, r):
-    for ix in range(0,numpy.size(r)):
-        uM[i,ix] = r[ix]
-def vj(j):
-    return numpy.reshape(numpy.matrix(vM[:,int(j)]), (d,1))
-def setVj(i, r):
-    for ix in range(0,numpy.size(r)):
-        vM[ix,i] = r[ix]
-def Mij(i,j):
-    return M[int(i),int(j)]
-
-for i in range(0,N1):
-    setUi(i, numpy.random.normal(0.0,1.0, d))
-for j in range(0,N2):
-    setVj(j, numpy.random.normal(0.0,1.0, d))
-
+    
 def I(n):
     return numpy.matrix(numpy.identity(n))
 def T(v):
@@ -91,49 +36,40 @@ def summ(l,r,c):
         return numpy.zeros((r,c))
     return numpy.sum(l, axis=0)
 
-fobj = open("objectives.csv", 'w')
-for iteration in range(0,iterations):    
-    for i in range(0,N1):
-        #u_i = \big[\lambda\sigma^2I + \sum_{j\in \omega ui}{v_jv^T}\big]*\big[\sum_{j\in \omega u_i}{M_{ij}v_j}\big]
-        sumofvjvjt = numpy.sum([numpy.dot(vj(j),T(vj(j)))
-            for j in omegaui(i)], 0)
-        item1 = (lambdaa*sigma2)*I(d)
-        itemiI = numpy.linalg.inv(item1 + sumofvjvjt)
-        item2 = summ([Mij(i,j)*vj(j) for j in omegaui(i)],d,1)
-        result = numpy.dot(itemiI, item2) 
-        setUi(i, result)
-    for j in range(0,N2):
-        sumofujujt = numpy.sum([numpy.dot(T(ui(i)),ui(i))
-            for i in omegavi(j)], 0)
-        item1 = (lambdaa*sigma2)*I(d)
-        itemiI = numpy.linalg.inv(item1 + sumofujujt)
-        item2 = summ([Mij(i,j)*ui(i) for i in omegavi(j)],1,d)
-        result = numpy.dot(itemiI, T(item2))
-        setVj(i, result)
-    def objfirst(i,j):
-        dot = (M[i,j] - numpy.dot(ui(i),vj(j))) 
-        dot = dot*dot
-        dot = (1.0/(2.0*sigma2))*dot
-        return dot
-    def objsecond(i):
-        l = numpy.linalg.norm(ui(i)) * numpy.linalg.norm(ui(i))
-        return (lambdaa/2.0)*l
-    def objthird(i):
-        l = numpy.linalg.norm(vj(i)) * numpy.linalg.norm(vj(i))
-        return (lambdaa/2.0)*l
-    objfirstpart = numpy.sum([objfirst(indices[0],indices[1])
-        for indices in omega()])
-    objsecondpart = numpy.sum([objsecond(i)
-        for i in range(0,N1)])
-    objthirdpart = numpy.sum([objthird(j)
-        for j in range(0,N2)])
-    obj = -(objfirstpart + objsecondpart + objthirdpart)
-    fobj.write('%.5f' % obj)
-    fobj.write("\n") 
-    if iteration == 9 or iteration == 24 or iteration == 49:
-        printMatrix("U-" + str(iteration+1) + ".csv", uM)
-        printMatrix("V-" + str(iteration+1) + ".csv", vM)
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 
-fobj.close()
-printMatrixShape("M",numpy.dot(uM,vM))
+
+dataM = numpy.loadtxt(open(data, "rb"), delimiter=",", skiprows=0)
+printMatrixShape("dataM", dataM)
+
+means = numpy.mean(dataM, axis=0)
+sds = numpy.std(dataM, axis=0)
+maxs = numpy.max(dataM, axis=0)
+mins = numpy.min(dataM, axis=0)
+
+colors = ["r","b","g","y","black"]
+
+def plotContour(i, centroid):
+    delta = 0.1
+    x = numpy.arange(mins[0], maxs[0], delta)
+    y = numpy.arange(mins[1], maxs[1], delta)
+    X, Y = numpy.meshgrid(x, y)
+    Z = mlab.bivariate_normal(X, Y, 1.0, 1.0, centroid[0], centroid[1])
+    plt.contour(X, Y, Z, colors=colors[i])
+    
+kn = 5
+centroids = [[numpy.random.normal(means[0],sds[0]),
+              numpy.random.normal(means[1],sds[1])]
+    for ki
+    in range(0,kn)]
+
+iterations = 1
+for i in range(0,iterations):
+    plt.scatter(dataM[:,0],dataM[:,1])
+    for x in range(0,kn):
+        plotContour(x, centroids[x])
+        plt.savefig("state_" + "{:0>2}".format(i))    
 print("ok")
