20b10f54396f428b2f063b375cbd85ad59f71d7e
Daniel Leite <Daniel Leite>
2017-04-15 18:46:35 +0100

    powershell script do cpp dev

---
+++ sources/Powershell/Modules/DevCpp/DevCpp.psm1
@@ -0,0 +1,97 @@
+﻿function Out-FileUtf8NoBom {
+
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory, Position=0)] [string] $LiteralPath,
+    [switch] $Append,
+    [switch] $NoClobber,
+    [AllowNull()] [int] $Width,
+    [Parameter(ValueFromPipeline)] $InputObject
+  )
+  [Environment]::CurrentDirectory = $PWD
+  $LiteralPath = [IO.Path]::GetFullPath($LiteralPath)
+  if ($NoClobber -and (Test-Path $LiteralPath)) { 
+    Throw [IO.IOException] "The file '$LiteralPath' already exists."
+  }
+  $sw = New-Object IO.StreamWriter $LiteralPath, $Append
+
+  $htOutStringArgs = @{}
+  if ($Width) {
+    $htOutStringArgs += @{ Width = $Width }
+  }
+   try {
+    $Input | Out-String -Stream @htOutStringArgs | % { $sw.WriteLine($_) }
+  } finally {
+    $sw.Dispose()
+  }
+}
+
+function New-CMakeEnv
+{
+    [CmdletBinding()]
+    param([Parameter(Position=0,Mandatory=$true)]$Name)
+    if([System.IO.File]::Exists("CMakeLists.txt")){
+        Write-Verbose "Environment already configured"
+        return
+    }
+
+    New-Item -Path CMakeLists.txt
+    "cmake_minimum_required (VERSION 2.6)
+project ($Name)
+
+set($($Name)_VERSION_MAJOR 0)
+set($($Name)_VERSION_MINOR 1)
+
+" | Out-FileUtf8NoBom  CMakeLists.txt | Out-Null
+
+    New-Item -Path build.ps1
+    "param([switch]`$SkipInstall, [switch]`$SkipGenerate, [string]`$Build = ""Debug"", [string]`$Run = ""Debug"")
+
+Push-Location
+cd (Split-Path `$PSCommandPath -Parent)
+
+if(`$SkipInstall.IsPresent -eq `$false){
+    Write-Information ""Installing...""
+    `$choco = command choco -EA SilentlyContinue
+    if(`$choco -eq `$null){
+        iwr https://chocolatey.org/install.ps1 -UseBasicParsing -Verbose | iex -Verbose
+    }
+
+    `$cmake = command cmake -EA SilentlyContinue
+    if(`$cmake -eq `$null){
+        choco install cmake -y -v
+    }
+    Write-Information ""Finished!""
+}
+
+if(`$SkipGenerate.IsPresent -eq `$false){
+    Write-Information ""Generating...""
+    rm .\.build -Recurse -Force -EA SilentlyContinue
+    mkdir .build | cd
+    mkdir msvc | cd
+    cmake ..\.. -G ""Visual Studio 14 2015 Win64""
+    Write-Information ""Finished!""
+}
+
+if([System.String]::IsNullOrEmpty(`$Build) -eq `$false){
+    Write-Information ""Building...""
+    cmake --build .\.build\msvc --config `$Build
+    Write-Information ""Finished!""
+}
+
+if([System.String]::IsNullOrEmpty(`$Run) -eq `$false){
+    .\.build\msvc\`$Run\$Name.exe
+}
+
+Pop-Location" | Out-FileUtf8NoBom  build.ps1 | Out-Null
+}
+
+function New-CMakeCppFile
+{
+    [CmdletBinding()]
+    param([Parameter(Position=0,Mandatory=$true)]$Name)
+
+    mkdir sources
+    New-Item "sources/$Name.cpp"
+    "add_executable(input sources/main.cpp)" | Out-FileUtf8NoBom CMakeLists.txt -Append
+}
