4e12c4b4075a68c0945e7d0121eca608af07d6a5
Daniel Leite <xunilrj@hotmail.com>
2017-05-25 03:38:51 -0600

    noumless ps module

--- sources/Powershell/Modules/Noumless/Noumless.psm1
+++ sources/Powershell/Modules/Noumless/Noumless.psm1
@@ -1,8 +1,41 @@
-﻿function get
+﻿function ine
 {
     [CmdletBinding()]
-    param($Path)
+    param([Parameter(Position=0)][string]$Expression,[Parameter(ValueFromPipeline=$true)]$Item)
+    process{
+        $pipe = [System.Collections.Generic.List[string]]::new()
+        $pipe.Add("`$_")
+        [System.Linq.Enumerable]::ToArray($Expression)|%{
+            if($_ -eq "g"){
+                $pipe.Add("get")
+            }elseif($_ -eq "s"){
+                $pipe.Add("stop")
+            }
+        }
 
-    $uri = [System.Uri]::new($path)
-    iex "Get-$($uri.Scheme)"
+        $cmd = [System.String]::Join("|", [System.Linq.Enumerable]::ToArray($pipe))
+        Write-Verbose $cmd
+        iex $cmd
+    }
+}
+
+function get
+{
+    [CmdletBinding()]
+    param([Parameter(ValueFromPipeline=$true)]$Path)
+    process{
+        $uri = [System.Uri]::new($Path)
+        iex "Get-$($uri.Scheme) $($uri.Segments[0])" | Add-Member -Name PSNoumlessScheme -MemberType NoteProperty -Value $uri.Scheme -PassThru
+    }
+}
+
+function stop
+{
+    [CmdletBinding()]
+    param([Parameter(ValueFromPipeline=$true)]$Item)
+    process{
+        if($Item.PSNoumlessScheme -ne $null){
+            iex "`$_ | Stop-$($Item.PSNoumlessScheme)"
+        }
+    }    
 }
