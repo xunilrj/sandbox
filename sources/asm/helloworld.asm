56b3d021f2486d905a32382289b9b7d71e50285f
Daniel Frederico Lins Leite <xunilrj@hotmail.com>
2018-02-12 12:05:28 +0000

    .

--- sources/asm/helloworld.asm
+++ sources/asm/helloworld.asm
@@ -1,21 +1,23 @@
  ;https://en.wikibooks.org/wiki/X86_Disassembly/Functions_and_Stack_Frames
- global _main
+ ;https://msdn.microsoft.com/en-us/library/zxk0tw93.aspx
+ ;https://msdn.microsoft.com/en-us/library/zkwh89ks.aspx
+ global Main
     extern  _GetStdHandle@4
     extern  _WriteFile@20
     extern  _ExitProcess@4
 
     section .text
 ; message, length
-_printMessage:
+PrintMessage:
     push ebp
     mov ebp, esp
     ;DWORD bytes
     sub esp, 4    
-    ; hStdOut = GetstdHandle(STD_OUTPUT_HANDLE)
+    ; hStdOut = __stdcall GetstdHandle(STD_OUTPUT_HANDLE)
     push    -11
     call    _GetStdHandle@4
     mov     ebx, eax
-    ; WriteFile(hstdOut, message, length(message), &bytes, 0);
+    ; __stdcall WriteFile(hstdOut, message, length(message), &bytes, 0);
     push    0
     lea     eax, [ebp-4]
     push    eax
@@ -27,19 +29,25 @@ _printMessage:
     mov esp, ebp
     pop ebp
     ret 4
-_main:
+Main:
     mov     ebp, esp
     sub     esp, 0
 
+    ; __cdecl PrintMessage(message1_end - message1, message1)
     push    (message1_end - message1)
     push    message1
-    call _printMessage
+    call PrintMessage
+    pop eax
+    pop eax
 
+    ; __cdecl PrintMessage(message2_end - message2, message2)
     push    (message2_end - message2)
     push    message2
-    call _printMessage
+    call PrintMessage
+    pop eax
+    pop eax
 
-    ; ExitProcess(0)
+    ; __stdcall ExitProcess(0)
     push    0
     call    _ExitProcess@4
 
