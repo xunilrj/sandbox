e224f626f7e171cf0540627b48c4039266833b88
Daniel Frederico Lins Leite <xunilrj@hotmail.com>
2018-05-12 11:55:24 +0100

    Peak Analysis using smoothed z-score

---
+++ sources/javascript/peakanalysis/main.R
@@ -0,0 +1,44 @@
+ThresholdingAlgo <- function(y,lag,threshold,influence) {
+  signals <- rep(0,length(y))
+  filteredY <- y[0:lag]
+  avgFilter <- NULL
+  stdFilter <- NULL
+  avgFilter[lag] <- mean(y[0:lag])
+  stdFilter[lag] <- sd(y[0:lag])
+  for (i in (lag+1):length(y)){
+    if (abs(y[i]-avgFilter[i-1]) > threshold*stdFilter[i-1]) {
+      if (y[i] > avgFilter[i-1]) {
+        signals[i] <- 1;
+      } else {
+        signals[i] <- -1;
+      }
+      filteredY[i] <- influence*y[i]+(1-influence)*filteredY[i-1]
+    } else {
+      signals[i] <- 0
+      filteredY[i] <- y[i]
+    }
+    avgFilter[i] <- mean(filteredY[(i-lag):i])
+    stdFilter[i] <- sd(filteredY[(i-lag):i])
+  }
+  return(list("signals"=signals,"avgFilter"=avgFilter,"stdFilter"=stdFilter))
+}
+
+# Data
+y <- c(1,1,1.1,1,0.9,1,1,1.1,1,0.9,1,1.1,1,1,0.9,1,1,1.1,1,1,1,1,1.1,0.9,1,1.1,1,1,0.9,
+       1,1.1,1,1,1.1,1,0.8,0.9,1,1.2,0.9,1,1,1.1,1.2,1,1.5,1,3,2,5,3,2,1,1,1,0.9,1,1,3,
+       2.6,4,3,3.2,2,1,1,0.8,4,4,2,2.5,1,1,1)
+
+lag       <- 5
+threshold <- 3.5
+influence <- 0.5
+
+# Run algo with lag = 30, threshold = 5, influence = 0
+result <- ThresholdingAlgo(y,lag,threshold,influence)
+
+# Plot result
+par(mfrow = c(2,1),oma = c(2,2,0,0) + 0.1,mar = c(0,0,2,1) + 0.2)
+plot(1:length(y),y,type="l",ylab="",xlab="") 
+lines(1:length(y),result$avgFilter,type="l",col="cyan",lwd=2)
+#lines(1:length(y),result$avgFilter+threshold*result$stdFilter,type="l",col="green",lwd=2)
+#lines(1:length(y),result$avgFilter-threshold*result$stdFilter,type="l",col="green",lwd=2)
+plot(result$signals,type="S",col="red",ylab="",xlab="",ylim=c(-1.5,1.5),lwd=2)
--- sources/javascript/peakanalysis/readme.md
+++ sources/javascript/peakanalysis/readme.md
@@ -2,8 +2,15 @@
 
 see: https://jsfiddle.net/7kYJu/5383/  
 
+# TODO
+
+[ ] C# version using Reactive Extensions  
+[ ] 2D Peak Analysis  
+[ ] n-D Peak Analysis?
+
 # Read More
 
 https://github.com/swizard0/smoothed_z_score  
 https://stackoverflow.com/questions/22583391/peak-signal-detection-in-realtime-timeseries-data/46956908#46956908  
 https://stackoverflow.com/questions/22583391/peak-signal-detection-in-realtime-timeseries-data/46956908#46956908  
+https://stackoverflow.com/questions/3684484/peak-detection-in-a-2d-array?noredirect=1&lq=1
