a5b328086156c38511154a3f7cc9edc7ef568f22
Daniel Frederico Lins Leite <xunilrj@hotmail.com>
2019-06-22 08:10:02 +0100

    pdf generation

---
+++ sources/csharp/pdf/PDF.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pdf
+{
+    public class PdfStream : PdfObject
+    {
+        public PdfPage Page { get; set; }
+
+        public Stream Stream { get; }
+
+        StreamWriter W;
+        public PdfStream()
+        {
+            Stream = new MemoryStream();
+            W = new StreamWriter(Stream, Encoding.ASCII);
+            W.AutoFlush = true;
+        }
+
+        public async Task AddValue (string str)
+        {
+            await W.WriteLineAsync(str);
+        }
+
+        public override async Task WriteTo(int i, StreamWriter w)
+        {
+            Stream.Position = 0;
+            await w.WriteLineAsync($@"<</Length {Stream.Length}>> stream");
+            await Stream.CopyToAsync(w.BaseStream);
+            await w.WriteLineAsync($@"endstream");
+            Stream.Position = Stream.Length - 1;
+        }
+
+        public PdfStream Raw(string raw) => WriteLine(raw);
+
+        public PdfStream BeginText() => WriteLine("BT");
+        public PdfStream EndText() => WriteLine("ET");
+        public PdfStream TextFont(PdfFont font, int size)
+        {
+            var pos = Page.GetResource(font);
+            return WriteLine($"/F{pos} {size} Tf");
+        }
+        public PdfStream TextPosition(int x, int y) => WriteLine($"{x} {y} Td");
+        public PdfStream TextPrint(string str) => WriteLine($"({str}) Tj");
+        public PdfStream TextNewLine() => WriteLine($"T*");
+        public PdfStream TextLeading(int leading) => WriteLine($"{leading} TL");
+
+
+
+        PdfStream WriteLine(string str)
+        {
+            W.WriteLine(str);
+            return this;
+        }
+    }
+
+    public class PdfFont : PdfObject
+    {
+        public PdfFont()
+        {
+            FontName = "Arial";
+        }
+
+        public string FontName { get; set; }
+        
+        public override async Task WriteTo(int i, StreamWriter w)
+        {
+            await w.WriteLineAsync($@"<</Type /Font
+/Subtype /Type1
+/Name /F1
+/BaseFont /{FontName}
+/Encoding /MacRomanEncoding
+>>");
+        }
+    }
+
+    public class PdfPage : PdfObject
+    {
+        PdfStream content;
+        public PdfStream Content
+        {
+            get => content;
+            set
+            {
+                content = value;
+                content.Page = this;
+            }
+        }
+
+        public int Width {get;set;}
+        public int Height {get;set;}
+
+        public PdfPage()
+        {
+            Width = 595;
+            Height = 842;
+        }
+
+
+        public override async Task WriteTo(int i, StreamWriter w)
+        {
+            await w.WriteLineAsync($@"<</Type /Page
+/Parent 2 0 R
+/MediaBox [0 0 {Width} {Height}]
+/Contents {Content.Index} 0 R
+/Resources <</ProcSet [/PDF /Text] /Font <<");
+
+            var fonts = Resources.Where(x => x.Key is PdfFont);
+            int fi = 1;
+            foreach(var f in fonts)
+            {
+                await w.WriteAsync($"/F{fi} {f.Key.Index} 0 R");
+                ++fi;
+            }
+            await w.WriteAsync(">> >> >>");
+        }
+
+        Dictionary<PdfObject, int> Resources = new Dictionary<PdfObject, int>();
+
+        public int GetResource(PdfObject obj)
+        {
+            if (Resources.TryGetValue(obj, out var i))
+                return i;
+            Resources.Add(obj, Resources.Count + 1);
+            return Resources.Count;
+        }
+    }
+
+    public class PdfPageTree : PdfObject
+    {
+        List<PdfPage> Pages;
+
+        public PdfPageTree()
+        {
+            Pages = new List<PdfPage>();
+        }
+
+        public override async Task WriteTo(int i, StreamWriter w)
+        {
+            await w.WriteAsync($@"<</Type /Pages /Kids [");
+            foreach(var p in Pages)
+            {
+                await w.WriteAsync($"{p.Index} 0 R ");
+            }
+            await w.WriteAsync($"] /Count {Pages.Count} >>");
+        }
+
+        public PdfPage AddPage(PdfPage page)
+        {
+            Pages.Add(page);
+            return page;
+        }
+    }
+
+    public abstract class PdfObject
+    {
+        public int Index { get; set; }
+        public abstract Task WriteTo(int i, StreamWriter w);
+    }
+
+    public class Pdf : IDisposable 
+    {
+        const int istart = 2;
+        StreamWriter W;
+
+        List<PdfObject> Objects { get; }
+        Dictionary<string, object> Trailer { get; }
+
+        public Pdf()
+        {
+            Trailer = new Dictionary<string, object>();
+            Objects = new List<PdfObject>();
+        }
+
+        public void AddObject(PdfObject obj)
+        {
+            obj.Index = Objects.Count + istart;
+            Objects.Add(obj);
+        }
+
+        public Stream Stream { get; private set; }
+               
+        public Task StartAsync()
+        {
+            Stream = new MemoryStream();
+            W = new StreamWriter(Stream, Encoding.ASCII);
+            W.AutoFlush = true;
+            return Task.CompletedTask;
+        }
+
+        public Task WriteHeader()
+        {
+            return W.WriteLineAsync("%PDF-1.4");
+        }
+
+        public Task WriteCatalog()
+        {
+            return W.WriteLineAsync(@"1 0 obj
+<</Type /Catalog
+/Pages 2 0 R
+>>
+endobj");
+        }
+
+        private long xrefpos = 0;
+
+        public async Task WriteXRef()
+        {
+            var pos = new List<long>();
+            int i = istart;
+            foreach (var obj in Objects)
+            {
+                pos.Add(Stream.Position);
+                await W.WriteLineAsync($"{i} 0 obj");
+                await obj.WriteTo(i, W);
+                await W.WriteLineAsync("endobj");
+
+                ++i;
+            }
+
+            xrefpos = Stream.Position;
+            await W.WriteLineAsync($@"xref
+0 {istart + pos.Count}
+0000000000 65535 f
+0000000009 00000 n
+0000000063 00000 n"
+);
+
+            foreach(var p in pos)
+            {
+                await W.WriteLineAsync($"{p:D10} 00000 n");
+            }
+        }
+
+
+        public async Task WriteTrailer()
+        {
+            await W.WriteLineAsync("trailer <<");
+            await W.WriteLineAsync($"/Size {istart + Objects.Count}");
+            await W.WriteLineAsync("/Root 1 0 R");
+
+            foreach (var kv in Trailer)
+            {
+                await W.WriteLineAsync($"/{kv.Key} {kv.Value}");
+
+            }
+            await W.WriteLineAsync(">>");
+        }
+
+        public Task WriteEOF()
+        {
+            return W.WriteLineAsync($"startxref {xrefpos}");
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    W.Dispose();
+                    Stream.Dispose();
+
+                    W = null;
+                    Stream = null;
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+
+    public static class PdfExtensions
+    {
+        public static async Task<PdfStream> AddStream(this Pdf pdf, string str = null)
+        {
+            var s = new PdfStream();
+            if(!string.IsNullOrEmpty(str))
+                await s.AddValue(str);
+            pdf.AddObject(s);
+            return s;
+        }
+
+        public static Task<PdfFont> AddFont(this Pdf pdf, string fontName = null)
+        {
+            var font = new PdfFont();
+            if(!string.IsNullOrEmpty(fontName))
+                font.FontName = fontName;
+            pdf.AddObject(font);
+            return Task.FromResult(font);
+        }
+
+        public static Task<PdfPage> AddPage(this Pdf pdf)
+        {
+            var page = new PdfPage();
+            pdf.AddObject(page);
+            return Task.FromResult(page);
+        }
+
+        public static Task<PdfPageTree> AddPageTree(this Pdf pdf)
+        {
+            var tree = new PdfPageTree();
+            pdf.AddObject(tree);
+            return Task.FromResult(tree);
+        }
+    }
+}
---
+++ sources/csharp/pdf/Program.cs
@@ -0,0 +1,82 @@
+﻿using SixLabors.Fonts;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace pdf
+{
+    class Program
+    {
+        //https://blog.idrsolutions.com/2013/01/understanding-the-pdf-file-format-overview/#helloworld
+        //https://blog.idrsolutions.com/2010/09/grow-your-own-pdf-file-part-1-pdf-objects-and-data-types/
+        //https://blog.idrsolutions.com/2010/09/grow-your-own-pdf-file-part-2-structure-of-a-pdf-file/
+        //https://blog.idrsolutions.com/2010/10/grow-your-own-pdf-part-2b-create-your-own-non-working-pdf/
+        //https://blog.idrsolutions.com/2010/10/grow-your-own-pdf-file-%E2%80%93-part-3-diy-blank-page/
+
+        //https://resources.infosecinstitute.com/pdf-file-format-basic-structure/
+
+        //http://www.printmyfolders.com/understanding-pdf
+        //http://www.printmyfolders.com/Home/understanding-the-portable-document-format-pdf-part-2
+
+        //https://www.adobe.com/content/dam/acom/en/devnet/acrobat/pdfs/PDF32000_2008.pdf
+        static async Task Main(string[] args)
+        {
+            using (var pdf = new Pdf())
+            {
+                var collection = new FontCollection();
+                var arialFamily = collection.Install("C:/Windows/Fonts/arial.ttf");
+                var arial = new Font(arialFamily, 20);
+
+                var str = "Hello World";
+                float w = 0;
+                foreach(var c in str)
+                {
+                    var glyph = arial.GetGlyph((int)c);
+                    w += glyph.Instance.AdvanceWidth / (72.0f / 96.0f);
+                }
+                //var shapes = SixLabors.Shapes.Temp.TextBuilder.GenerateGlyphs(text, new Primitives.PointF(50f, 4f), new RendererOptions(font, 72));
+
+
+                var tree = await pdf.AddPageTree();
+                var page = tree.AddPage(await pdf.AddPage());
+                var font = await pdf.AddFont();
+                page.Content = await pdf.AddStream();
+                page.Content.BeginText()
+                    .Raw("/P << /MCID 0 >>")
+                    .Raw("BDC")
+                    .TextFont(font, 20)
+                    .Raw("1 2 3 4 5 6 Tm")
+                    //.TextLeading(20)
+                    //.TextPosition(0, 822)
+                    .TextPrint(@"Something which is extremely hot, in any sense. Hot weather, sexual arousal, one who is wanted by the police, etc. are all described as ""hotter than..."" or "" as hot as a fresh fucked fox in a forest fire")
+                    //.TextNewLine()
+                    .TextPosition((int)w, 0)
+                    .TextPrint("Again!")
+                    .Raw("EMC")
+                .EndText();
+                var page2 = tree.AddPage(await pdf.AddPage());
+                page2.Content = await pdf.AddStream();
+                page2.Content.BeginText()
+                   .TextFont(font, 20)
+                   .TextPosition(0, 0)
+                   .TextPrint("Hello World2!")
+               .EndText();
+                await pdf.StartAsync();
+
+                await pdf.WriteHeader();
+                await pdf.WriteCatalog();
+
+                await pdf.WriteXRef();
+                await pdf.WriteTrailer();
+                await pdf.WriteEOF();
+
+                using (var f = File.OpenWrite("./a.pdf"))
+                {
+                    pdf.Stream.Position = 0;
+                    await pdf.Stream.CopyToAsync(f);
+                    await f.FlushAsync();
+                }
+            }
+        }
+    }
+}
