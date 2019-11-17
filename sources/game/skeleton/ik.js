2af42c200f0d3647ea82e8337c15b438b8b44e61
Daniel Frederico Lins Leite <xunilrj@hotmail.com>
2019-11-17 21:05:25 +0000

    ik

--- sources/game/skeleton/ik.js
+++ sources/game/skeleton/ik.js
@@ -1,5 +1,16 @@
 import './once.js';
 import mouseCanvas  from './mouseCanvas.js';
+import { Matrix, pseudoInverse } from 'ml-matrix'
+function crossProduct(a, b, out)
+{
+    out = out || [];
+    var ax = a[0], ay = a[1], az = a[2],
+        bx = b[0], by = b[1], bz = b[2];
+    out[0] = ay * bz - az * by;
+    out[1] = az * bx - ax * bz;
+    out[2] = ax * by - ay * bx;
+    return out;
+}
 
 const canvas = document.querySelector("canvas");
 
@@ -88,12 +99,13 @@ function drawCross(x,y,r)
 function drawAngle(x,y,r,angle)
 {
     [x,y] = toCanvas(x,y);
+    var startAngle = getRotate();
 
     ctx.fillStyle = "lightblue";
     ctx.strokeStyle = "blue";
     ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.arc(x, y, r, 0, -angle, true);
+        ctx.moveTo(x, y);
+        ctx.arc(x, y, r, -(startAngle-angle), -(startAngle), true);
     ctx.closePath();
     ctx.fill();
     var oldLineWidth = ctx.lineWidth;
@@ -121,14 +133,54 @@ function drawBone(x, r)
 }
 function drawBones(bones)
 {
+    let eff = bones.filter(x => x.isEffector)[0].effector_world;
     bones.forEach((x,i) => {
         var w = x.world;
-        drawAngle(w.x, w.y, 10, x.angle);
-        pushRotate(x.angle);
+        pushRotate(x.world.angle + x.angle);
+            drawAngle(w.x, w.y, 10, x.angle);
             drawCircle(w.x, w.y, 20);
             drawCross(w.x, w.y, 20);
             drawBone(x, 20);
         popRotate();
+
+        if(x.drawCircle)
+        {
+            var xl = eff.x - x.world.x;
+            var yl = eff.y - x.world.y;
+            var l = Math.sqrt(xl**2+yl**2);
+
+            var w = x.world;
+            let [cx,cy] = toCanvas(w.x,w.y);
+            ctx.setLineDash([5, 5]);
+            ctx.beginPath();                
+                ctx.arc(cx, cy, l, 0, 2*Math.PI, true);            
+            ctx.stroke();
+            ctx.setLineDash([]);
+
+            if(j)
+            {
+                eff = bones.filter(x => x.isEffector)[0];
+                let [cx,cy] = toCanvas(
+                    eff.effector_world.x,
+                    eff.effector_world.y);
+
+
+                var l = Math.sqrt(
+                    (j.get(0, i)**2) +
+                    (j.get(1, i)**2)
+                );                
+                var oldStrokeStyle = ctx.strokeStyle;
+                ctx.strokeStyle = "red";
+                ctx.beginPath();                
+                    ctx.moveTo(cx,  cy);
+                    ctx.lineTo(
+                        cx + (j.get(0, i)/l)*100,  
+                        cy + (j.get(1, i)/l)*100
+                    );
+                ctx.stroke();
+                ctx.strokeStyle = oldStrokeStyle;
+            }
+        }
     });
 }
 
@@ -168,6 +220,82 @@ let skeleton = [
     {children:[1]}    
 ]
 
+var j;
+var jpath = [];
+function updateJ()
+{
+    let recordPath = false;
+    if(jpath.length == 0) recordPath = true;
+    if(recordPath) jpath = [];
+
+    let eff = bones.filter(x => x.isEffector)[0];
+    //how much theta change, change effector pos?
+
+    let steps = 10;
+    var deffx = target[0] - eff.effector_world.x;
+    var deffy = target[1] - eff.effector_world.y;
+    var distance = Math.sqrt(
+        deffx**2 +
+        deffy**2
+    );
+    while(steps > 0 && Math.abs(distance) > 5) 
+    {
+        steps--;
+        // 2xbones
+        j = Matrix.zeros(2, bones.length);
+        for(var i = bones.length - 1; i >= 0; --i)
+        {        
+            var b = bones[i];
+
+            var dx = eff.effector_world.x - b.world.x;
+            var dy = eff.effector_world.y - b.world.y;
+            var cp = crossProduct([0,0,1],[dx,dy,0]);
+
+            j.set(0, i, -cp[0]);
+            j.set(1, i, -cp[1]);
+        }
+
+        // bonesx2
+        let jPlus = pseudoInverse(j);
+
+        var deffx = target[0] - eff.effector_world.x;
+        var deffy = target[1] - eff.effector_world.y;
+        //bonesx2 * 2x1 = bonesx1
+        var dtheta = jPlus.mmul(new Matrix([[deffx],[deffy]]));
+
+        var bonespos = [];
+        for(var i = 0; i < bones.length; ++i)
+        { 
+            var b = bones[i];
+            var a = dtheta.get(i, 0) * 0.1;
+            var max = 0.01;
+            if(a > max) a = max;
+            if(a < -max) a = -max;
+            b.angle += a;
+            bonespos.push(b.angle);
+        }
+        jpath.push(bonespos);
+
+        updateBones(skeleton, bones, {x:0,y:0,angle:0});
+
+        deffx = target[0] - eff.effector_world.x;
+        deffy = target[1] - eff.effector_world.y;
+        distance = Math.sqrt(deffx**2 + deffy**2);
+
+        
+    } 
+
+    // document.getElementById("stepDebug").innerText =
+    //     JSON.stringify({bones,j, angles});
+}
+
+let ikEnabled = false;
+document.getElementById("ikstep")
+.addEventListener("click", e => {
+    jpath = [];
+    ikEnabled = e.target.checked;
+});
+
 function updateBone(skeleton, bones, i, world)
 {
     var bone = bones[i];
@@ -200,11 +328,52 @@ function updateBones(skeleton, bones, world)
 function render() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
 
+    var [cx,cy] = toCanvas(0,0);
+    var [ux,uy] = toCanvas(100,100);
+    ctx.beginPath();
+        ctx.moveTo(cx,cy);
+        ctx.lineTo(ux,cy);
+    ctx.stroke();
+    ctx.beginPath();
+        ctx.moveTo(cx,cy);
+        ctx.lineTo(cx,uy);
+    ctx.stroke();
+
     updateBones(skeleton, bones, {x:0,y:0,angle:0});
+    if(ikEnabled)
+        updateJ();
     drawBones(bones);
     
     drawPathTarget(bones, target);
     drawTarget(target);
+
+    //draw state space    
+    ctx.beginPath();
+        ctx.rect(580, 20, 200, 200);
+    ctx.stroke();
+    function toAngleSpace(t1)
+    {
+        let x = t1 / (2*Math.PI);
+        if(x < 0) x += 1;
+        if(x > 1) x -= 1;
+        return x;
+    }
+    ctx.beginPath();
+        ctx.arc(
+            580 + (toAngleSpace(bones[0].angle) * 200), 
+             20 + (toAngleSpace(bones[1].angle) * 200),
+            5, 0, 2*Math.PI, true);
+    ctx.stroke();
+    jpath.forEach(x => {
+        ctx.beginPath();
+            ctx.arc(
+                580 + (toAngleSpace(x[0]) * 200), 
+                20 + (toAngleSpace(x[1]) * 200),
+                5, 0, 2*Math.PI, true);
+        ctx.stroke();
+    });
+    ctx.fillText("θ2",565,215);
+    ctx.fillText("θ1",580,235);
 
     requestAnimationFrame(render);
 }
@@ -221,7 +390,14 @@ bones.forEach((x,i) => {
         bones[i].angle = parseFloat(input.value / 180*Math.PI);
         label.innerText = input.value;
     });
+    
     let root = document.createElement("div");
+    root.addEventListener("mouseenter", x => {
+        bones[i].drawCircle = true;
+    });
+    root.addEventListener("mouseleave", x => {
+        bones[i].drawCircle = false;
+    });
     root.append(input);
     root.append(label);
 
---
+++ sources/game/skeleton/readme.md
@@ -0,0 +1,12 @@
+# Bones, Deformed Mesh and Inverse Kinematics
+
+##
+
+https://www.math.ucsd.edu/~sbuss/ResearchWeb/ikmethods/SdlsPaper.pdf
+
+Pseudo-Inverse
+
+Resolved Motion Rate Control of Manipulators and Human Prostheses
+https://ieeexplore.ieee.org/document/4081862  
+PDF - https://www.researchgate.net/publication/3298017_Resolved_Motion_Rate_Control_of_Space_Manipulators_with_Generalized_Jacobian_Matrix  
+
