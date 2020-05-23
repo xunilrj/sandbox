84c7e0e29754fc9665802c80b4b224f876d3ab96
Daniel Leite <1985.daniel@gmail.com>
2020-05-23 09:41:47 +0100

    .

--- courses/MITx/MITx 6.86x Machine Learning with Python-From Linear Models to Deep Learning/project0/get_sum_metrics.py
+++
@@ -1,9 +0,0 @@
-def get_sum_metrics(predictions, metrics=[]):
-    for i in range(3):
-        metrics.append(lambda x: x + i)
-
-    sum_metrics = 0
-    for metric in metrics:
-        sum_metrics += metric(predictions)
-
-    return sum_metrics
--- courses/MITx/MITx 6.86x Machine Learning with Python-From Linear Models to Deep Learning/project0/scalar_function.py
+++
@@ -1,8 +0,0 @@
-def scalar_function(x, y):
-    """
-    Returns the f(x,y) defined in the problem statement.
-    """
-    if (x<=y):
-        return x*y
-    else:
-        return x/y
