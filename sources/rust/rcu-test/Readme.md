f28f12f03fdc0bbadec351d9a75aee211036fcc6
Daniel Leite <1985.daniel@gmail.com>
2020-06-24 08:50:35 +0100

    rcu - fifo - rust

---
+++ sources/rust/rcu-test/Readme.md
@@ -0,0 +1,58 @@
+# Concurrent Data Structures
+
+## Memory Models
+
+https://preshing.com/20120710/memory-barriers-are-like-source-control-operations
+https://preshing.com/20120625/memory-ordering-at-compile-time
+https://preshing.com/20120913/acquire-and-release-semantics
+
+## Fifo
+
+### Developing
+
+### Testing
+
+The hardest part for concurrent data structures is testing. There are SO many cases, and variations, that is almost certain, that we, developers, will forget something.
+
+The standard approach is:
+
+1 - Test the serial case. It must work with one thread;
+2 - Create the concurrent test using threads. Run it million of times and hope to catch all possibilities.
+
+Although these are nice approaches, and you CAN deliver running software like that. A lot of bright mings ind the field of Computer Science tried to think in better approaches. One of these minds was: Brian Norris and Brian Demsky.
+
+They wrote a paper called: "CDSCHECKER: Checking Concurrent Data Structures Written with C/C++ Atomics"  
+http://demsky.eecs.uci.edu/publications/c11modelcheck.pdf  
+
+```
+While it is possible to use a formal specification of the
+C/C++ memory model [8] to prove code correct, experience
+suggests that most software developers are unlikely to do so
+(e.g., because they lack expertise or time).
+
+We present a new approach for exhaustively exploring the
+behaviors of code under the C/C++ memory model, based
+on stateless model-checking.
+
+Stateless model-checkers
+typically explore a program’s possible behaviors—or state
+space—by repeatedly executing the program under different
+thread interleavings.
+
+Thus, state-of-the-art model-checking rests on a class
+of optimization techniques known as dynamic partial-order
+reduction (DPOR) [20]. The DPOR algorithm can reduce
+the explored state space (e.g., two
+concurrent stores to the same object conflict, whereas two
+loads do not).
+
+Conflict points are recorded in a backtracking set, so
+that the exploration can return (or backtrack) to
+the recorded program point during a future execution and
+attempt a different thread interleaving.
+
+At runtime, CDSCHECKER schedules program
+fragments sequentially and determines the values returned
+by atomic memory operations
+```
+
---
+++ sources/rust/rcu-test/src/lib.rs
@@ -0,0 +1,136 @@
+use std::fmt::Debug;
+
+#[cfg(loom)]
+pub(crate) use loom::sync::atomic::{AtomicPtr, Ordering};
+//#[cfg(loom)]
+//pub(crate) use loom::sync::Arc;
+
+#[cfg(not(loom))]
+pub(crate) use std::sync::atomic::{AtomicPtr, Ordering};
+//#[cfg(not(loom))]
+pub(crate) use std::sync::Arc;
+
+use std::boxed::Box;
+
+#[derive(Debug)]
+pub struct FifoNode<T> {
+    value: T,
+    next: AtomicPtr<FifoNode<T>>,
+}
+
+impl<T> FifoNode<T> {
+    fn unwrap(self) -> T {
+        self.value
+    }
+}
+
+#[derive(Debug)]
+pub struct Fifo<T: Debug> {
+    head: Arc<AtomicPtr<FifoNode<T>>>,
+}
+
+unsafe impl<T: Debug> Send for Fifo<T> {}
+unsafe impl<T: Debug> Sync for Fifo<T> {}
+impl<T: Debug> Clone for Fifo<T> {
+    fn clone(&self) -> Self {
+        Self {
+            head: self.head.clone(),
+        }
+    }
+}
+impl<T: Debug> Default for Fifo<T> {
+    fn default() -> Self {
+        Self {
+            head: Arc::new(AtomicPtr::default()),
+        }
+    }
+}
+
+impl<'a, T: Debug> Fifo<T> {
+    pub fn new() -> Self {
+        Default::default()
+    }
+    pub fn push(&self, value: T) {
+        let mut next_head = Box::new(FifoNode::<T> {
+            value,
+            next: AtomicPtr::default(),
+        });
+        loop {
+            let current_head = self.head.load(Ordering::Relaxed);
+            next_head.next = AtomicPtr::new(current_head);
+            match self.head.compare_exchange(
+                current_head,
+                &mut *next_head,
+                Ordering::SeqCst,
+                Ordering::SeqCst,
+            ) {
+                Ok(_) => {
+                    Box::leak(next_head);
+                    break;
+                }
+                Err(_) => {}
+            }
+        }
+    }
+
+    pub fn peek(&'a self) -> Option<&'a T> {
+        let h = self.head.load(Ordering::Relaxed);
+        if h.is_null() {
+            None
+        } else {
+            let node = unsafe { &*h };
+            Some(&node.value)
+        }
+    }
+
+    pub fn pop(&self) -> Option<T> {
+        loop {
+            let current_head_ptr = self.head.load(Ordering::SeqCst);
+            if current_head_ptr.is_null() {
+                break None;
+            } else {
+                let current_head = unsafe { &*current_head_ptr };
+                let current_head_next_ptr = current_head.next.load(Ordering::SeqCst);
+                match self.head.compare_exchange(
+                    current_head_ptr,
+                    current_head_next_ptr,
+                    Ordering::SeqCst,
+                    Ordering::SeqCst,
+                ) {
+                    Ok(_) => {
+                        let current_head = unsafe { Box::from_raw(current_head_ptr) };
+                        break Some(current_head.unwrap());
+                    }
+                    Err(_) => {}
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+#[cfg(loom)]
+mod tests {
+    #[test]
+    fn t1push_t2pop() {
+        pretty_env_logger::init();
+        loom::model(|| {
+            let q = crate::Fifo::<u32>::new();
+            let q1 = q.clone();
+            let q2 = q.clone();
+            let j1 = loom::thread::spawn(move || {
+                q1.push(1);
+            });
+            let j2 = loom::thread::spawn(move || loop {
+                match q2.pop() {
+                    None => {}
+                    Some(v) => break v,
+                }
+                loom::thread::yield_now();
+            });
+
+            j1.join().unwrap();
+            assert_eq!(1, j2.join().unwrap());
+        });
+    }
+}
