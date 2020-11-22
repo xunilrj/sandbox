4a26f2c8182a99d380b9bee36e01cc2d6e43248c
Daniel Leite <xunilrj@hotmail.com>
2020-11-22 19:50:39 +0000

    .

--- sources/rust/disk-speed/main.cpp
+++ sources/rust/disk-speed/main.cpp
@@ -1,5 +1,17 @@
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <unistd.h>
+#include <fcntl.h> 
+#include <cstdint>
 
 int main(int argc, char **argv)
 {
+    uint8_t byte = 0;
+
+    auto h = creat(".cpp", S_IRWXU);
+    write(h, &byte, 1);
+    fdatasync(h); 
+    close(h);
+
     return 0;
 }
